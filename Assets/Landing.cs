23a2592db09ac736862f83b8c2f6644bf5d75f15
Brent Allen <labstudios@gmail.com>
2016-02-24 19:18:59 -0700

    GUI interface Week 7

---
+++ Assets/Landing.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace CompleteProject{
+	public class Landing : MonoBehaviour {
+
+		public Button playBtn;
+		public Button exitBtn;
+
+		// Use this for initialization
+		void Awake () {
+
+			playBtn.GetComponent<Button> ().onClick.AddListener (() => {playGame();});
+			exitBtn.GetComponent<Button> ().onClick.AddListener (() => {quitGame();});
+		
+		}
+
+		private void playGame(){
+			SceneManager.LoadScene (1);
+		}
+
+		private void quitGame(){
+			Application.Quit ();
+		}
+	}
+}
